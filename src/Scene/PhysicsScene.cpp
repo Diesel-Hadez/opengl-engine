599b1c4be02e0c20849373154ac3cf3744f99788
Diesel-Hadez <32006772+Diesel-Hadez@users.noreply.github.com>
2022-06-29 13:15:08 +0800

    Used FetchContent using CMake

--- src/Scene/PhysicsScene.cpp
+++ src/Scene/PhysicsScene.cpp
@@ -44,17 +44,17 @@ void PhysicsScene::Update() {
 			m_FPCamera->ProcessKeyboard(FPCamera::Movement::DOWN, timeStep);
 		
 		if (glfwGetKey(Game::m_Window, GLFW_KEY_I) == GLFW_PRESS)
-			m_CuboidBody->applyForceToCenterOfMass(r3d::Vector3(10,0,0));
+			m_CuboidBody->applyWorldForceAtCenterOfMass(r3d::Vector3(10,0,0));
 		if (glfwGetKey(Game::m_Window, GLFW_KEY_K) == GLFW_PRESS)
-			m_CuboidBody->applyForceToCenterOfMass(r3d::Vector3(-10,0,0));
+			m_CuboidBody->applyWorldForceAtCenterOfMass(r3d::Vector3(-10,0,0));
 		if (glfwGetKey(Game::m_Window, GLFW_KEY_L) == GLFW_PRESS)
-			m_CuboidBody->applyForceToCenterOfMass(r3d::Vector3(0,0,10));
+			m_CuboidBody->applyWorldForceAtCenterOfMass(r3d::Vector3(0,0,10));
 		if (glfwGetKey(Game::m_Window, GLFW_KEY_J) == GLFW_PRESS)
-			m_CuboidBody->applyForceToCenterOfMass(r3d::Vector3(0,0,-10));
+			m_CuboidBody->applyWorldForceAtCenterOfMass(r3d::Vector3(0,0,-10));
 		if (glfwGetKey(Game::m_Window, GLFW_KEY_U) == GLFW_PRESS)
-			m_CuboidBody->applyForceToCenterOfMass(r3d::Vector3(0,50,0));
+			m_CuboidBody->applyWorldForceAtCenterOfMass(r3d::Vector3(0,50,0));
 		if (glfwGetKey(Game::m_Window, GLFW_KEY_O) == GLFW_PRESS)
-			m_CuboidBody->applyForceToCenterOfMass(r3d::Vector3(0,-50,0));
+			m_CuboidBody->applyWorldForceAtCenterOfMass(r3d::Vector3(0,-50,0));
 		
 		
 		
