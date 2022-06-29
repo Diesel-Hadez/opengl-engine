5a6862989f6039a7c8e1e0769aaa51a8e0ccf72d
Diesel-Hadez <32006772+Diesel-Hadez@users.noreply.github.com>
2022-06-29 14:05:21 +0800

    Got rid of reactphysics3d testing in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,7 @@
 #include "Game.h"
-#include <reactphysics3d/reactphysics3d.h>
 #include <iostream>
 
 auto main(int argc, char* argv[]) -> int {
-    using namespace reactphysics3d;
-    PhysicsCommon physicsCommon;
-
-    PhysicsWorld* world = physicsCommon.createPhysicsWorld();
-
-    Vector3 position(0, 20, 0);
-
-    Quaternion orientation = Quaternion::identity();
-
-    Transform transform(position, orientation);
-
-    RigidBody* body = world->createRigidBody(transform);
-
-    const decimal timeStep = 1.0f / 60.0f;
-
-    for (int i = 0; i < 20; i++) {
-        world->update(timeStep);
-
-        const Transform& transform = body->getTransform();
-        const Vector3&   position  = transform.getPosition();
-
-        std::cout << "Body Position: (" << position.x << ", " << position.y << ", " << position.z
-                  << ")" << std::endl;
-    }
-
     try {
         Game::GetInstance().Run();
     } catch (const std::exception& e) {
