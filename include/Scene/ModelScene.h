7394be57607e4da3591d5453d2014f0f74897a9e
Diesel-Hadez <32006772+Diesel-Hadez@users.noreply.github.com>
2023-06-20 14:59:56 +0800

    Added seleection outline in ModelScene

--- include/Scene/ModelScene.h
+++ include/Scene/ModelScene.h
@@ -13,12 +13,14 @@ class Position;
 class ModelScene : public Scene {
 private:
     std::unique_ptr<Shader>   m_Shader;
+    std::unique_ptr<Shader>   m_HighlightShader;
     std::unique_ptr<Shader>   m_LightCubeShader;
     std::unique_ptr<FPCamera> m_FPCamera;
     std::unique_ptr<Position> m_LightGPUData;
     Model::Model              m_Model;
     float                     m_DeltaTime;
     glm::vec3                 lampPos;
+    bool                      m_Highlight;
 
 public:
     virtual void Update() override;
--- src/Scene/ModelScene.cpp
+++ src/Scene/ModelScene.cpp
@@ -5,6 +5,7 @@
 #include "GPUData/Position.h"
 #include "SampleVertices.h"
 
+#include <GL/gl.h>
 #include <glm/gtc/matrix_transform.hpp>
 
 #include <GLFW/glfw3.h>
@@ -12,6 +13,9 @@
 namespace {
     float currentFrame = 0;
     float lastFrame    = 0;
+
+    bool       highlightPressed = false;
+    const auto HIGHLIGHT_KEY    = GLFW_KEY_X;
 }  // namespace
 void ModelScene::Update() {
     currentFrame = glfwGetTime();
@@ -48,6 +52,14 @@ void ModelScene::Update() {
     if (glfwGetKey(Game::m_Window, GLFW_KEY_O) == GLFW_PRESS)
         lampPos -= top;
 
+    if (!highlightPressed && glfwGetKey(Game::m_Window, HIGHLIGHT_KEY) == GLFW_PRESS) {
+        highlightPressed = true;
+    }
+    if (highlightPressed && glfwGetKey(Game::m_Window, HIGHLIGHT_KEY) == GLFW_RELEASE) {
+        highlightPressed = false;
+        m_Highlight      = !m_Highlight;
+    }
+
     if (glfwGetKey(Game::m_Window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         Game::GetInstance().PopScene();
 }
@@ -55,8 +67,16 @@ void ModelScene::Update() {
 void ModelScene::Render() {
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    glEnable(GL_STENCIL_TEST);
+    glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    glStencilMask(0x00);
+
+    glStencilFunc(GL_ALWAYS, 1, 0xFF);
+    glStencilMask(0xFF);
 
     m_Shader->Use();
     // m_CubeTexture->Bind(GL_TEXTURE0);
@@ -89,8 +109,23 @@ void ModelScene::Render() {
     m_Shader->Set<glm::vec3>("light.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
     m_Shader->Set<glm::vec3>("light.specular", glm::vec3(1.f, 1.f, 1.f));
     m_Shader->Set<glm::vec3>("light.position", lampPos);
-
     m_Model.Draw(*m_Shader);
+
+    glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+    glStencilMask(0x00);
+    glDisable(GL_DEPTH_TEST);
+    m_HighlightShader->Use();
+    model = glm::scale(model, glm::vec3(1.01));
+    m_HighlightShader->Set<glm::mat4>("model", model);
+    m_HighlightShader->Set<glm::mat4>("projection", projection);
+    m_HighlightShader->Set<glm::mat4>("view", view);
+    if (m_Highlight) {
+        m_Model.Draw(*m_HighlightShader);
+    }
+
+    glStencilMask(0xFF);
+    glStencilFunc(GL_ALWAYS, 1, 0xFF);
+    glEnable(GL_DEPTH_TEST);
 
     // m_CubeGPUData->Bind();
     //
@@ -111,11 +146,14 @@ ModelScene::ModelScene()
                                         "../resources/model_loading.fs"))
     , m_LightCubeShader(std::make_unique<Shader>("../resources/lightingcube1.vs",
                                                  "../resources/lightingcube1.fs"))
+    , m_HighlightShader(std::make_unique<Shader>("../resources/lightingcube1.vs",
+                                                 "../resources/lightingcube1.fs"))
     , m_Model("../resources/models/backpack/backpack.obj")
     , m_FPCamera(std::make_unique<FPCamera>(glm::vec3(0.0f, 0.0f, 3.0f)))
     , m_DeltaTime(0.0f)
     , m_LightGPUData(std::make_unique<Position>())
-    , lampPos(glm::vec3(0.f, 1.f, 0.5f)) {
+    , lampPos(glm::vec3(0.f, 1.f, 0.5f))
+    , m_Highlight(false) {
     m_SceneName = "ModelScene";
     m_LightGPUData->Prepare(const_cast<float*>(Cube), sizeof(Cube));
 }
